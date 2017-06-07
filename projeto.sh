f7e1ce010615945633a646086b02736f148178bb
guedesbgeorge <guedesbgeorge@gmail.com>
2017-06-07 09:33:26 -0300

    Chamando maven pra compilar e testar o junit

--- projeto.sh
+++ projeto.sh
@@ -1,10 +1,14 @@
-# TODO: Lidar com casos de o git não estar instalado e do diretório de destino do clone não estar vazio.
+# TODO: 1) Lidar com casos de o git não estar instalado, 2) do diretório de destino do clone (DEV/<PASTA_DO_PROJETO>) não estar vazio,
+# 3) de não ter o java instalado, 4) de não ter a variavel de ambiente JAVA_HOME setada, 5) de não ter o maven ou o junit instalado.
 
 # Criando diretórios DEV, TEST, UAT, PRODUCTION
 mkdir -v DEV TEST UAT PRODUCTION
 
-# Perguntando URL do projeto (digitar https://github.com/jfsc/spring-petclinic.git)
+# Perguntando URL do projeto (digitar https://github.com/jfsc/spring-petclinic.git) e clonando no diretório DEV
 echo "Digite a URL do projeto: "
 read url
 cd DEV
-git clone $url
+git clone $url && cd $(basename $_ .git)
+
+# Compilando e testando o junit
+mvn clean install
