b725cab890945851d89056272cddb1c676014445
Henrique Vicente <henriquevicente@gmail.com>
2017-06-07 11:52:42 -0300

    Adding checks.

--- projeto.sh
+++ projeto.sh
@@ -1,14 +1,67 @@
-# TODO: 1) Lidar com casos de o git não estar instalado, 2) do diretório de destino do clone (DEV/<PASTA_DO_PROJETO>) não estar vazio,
-# 3) de não ter o java instalado, 4) de não ter a variavel de ambiente JAVA_HOME setada, 5) de não ter o maven ou o junit instalado.
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+function checkGitExists() {
+    (which git >> /dev/null) && ec=$? || ec=$?
+
+  if [ ! $ec -eq 0 ] ; then
+    >&2 echo "git was not found on your system"
+    exit 1
+  fi
+}
+
+function checkJavaExists() {
+    (which java >> /dev/null) && ec=$? || ec=$?
+
+  if [ ! $ec -eq 0 ] ; then
+    >&2 echo "Java was not found on your system :)"
+    exit 1
+  fi
+}
+
+function checkMavenExists() {
+    (which mvn >> /dev/null) && ec=$? || ec=$?
+
+  if [ ! $ec -eq 0 ] ; then
+    >&2 echo "Maven (https://maven.apache.org/) was not found on your system"
+    exit 1
+  fi
+}
+
+function cloneGitIfNotExistsAndRunTests() {
+  reponame=${url##*/}
+  reponame=${reponame%.git}
+  
+  if [ ! -d $reponame ] ; then
+    git clone $url $reponame
+    cd $reponame
+    mvn clean install
+    cd ..
+  else
+    >&2 echo "Project already exists"
+    exit 1
+  fi
+}
+
+function checkJavaHomeEnvSet() {
+    if [ ! -z ${var+x} ]; then
+    >&2 echo "JAVA_HOME environment variable is not set"
+    exit 1
+    fi
+}
+
+checkGitExists
+checkJavaExists
+checkJavaHomeEnvSet
+checkMavenExists
+
+# Perguntando URL do projeto (digitar https://github.com/jfsc/spring-petclinic.git)
+read -p "Project URL: " url
 
 # Criando diretórios DEV, TEST, UAT, PRODUCTION
-mkdir -v DEV TEST UAT PRODUCTION
+mkdir -p DEV TEST UAT PRODUCTION
 
-# Perguntando URL do projeto (digitar https://github.com/jfsc/spring-petclinic.git) e clonando no diretório DEV
-echo "Digite a URL do projeto: "
-read url
 cd DEV
-git clone $url && cd $(basename $_ .git)
-
-# Compilando e testando o junit
-mvn clean install
+cloneGitIfNotExistsAndRunTests
